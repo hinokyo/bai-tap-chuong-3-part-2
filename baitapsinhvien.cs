a4e47a84252043150084026a0259880a76f9f66f
hinokyo <101516030+hinokyo@users.noreply.github.com>
2022-03-21 01:31:35 +0700

    Add files via upload

---
+++ baitapsinhvien.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinh_Vien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            sinhvien sv1 = new sinhvien();
+            sinhvien sv2 = new sinhvien();
+            Console.WriteLine("Thong tin sinh vien thu nhat la: ");
+            sv1.nhap();
+            sv1.xuat();
+            Console.WriteLine("Thong tin sinh vien thu hai la: ");
+            sv2.nhap();
+            sv2.xuat();
+        }
+    }
+}
